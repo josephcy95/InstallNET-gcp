e4b14f2ac9fb694b637224174467fde6298e9689
Joseph Yap <josephcy95@gmail.com>
2020-10-31 06:00:23 +0800

    Update InstallNET-gcp2.sh

--- InstallNET-gcp2.sh
+++ InstallNET-gcp2.sh
@@ -9,4 +9,4 @@ echo $gcpinternalIP
 gcpgatewayIP=$(ip r | awk '/^def/{print $3}')
 echo $gcpgatewayIP
 
-wget --no-check-certificate -qO InstallNET.sh 'https://raw.githubusercontent.com/josephcy95/InstallNET-gcp/master/InstallNET.sh' && bash InstallNET.sh --ip-addr $gcpinternalIP --ip-mask 255.255.255.0 --ip-gate $gcpgatewayIP -dd 'https://www.googleapis.com/drive/v3/files/1O5IxVFIM_QQLNegejIlOh8aevSrIT5g5?alt=media&key=AIzaSyAbXp6Gu21SKql8AHFb9RWgX36F8Aap5Xw'
+wget --no-check-certificate -qO InstallNET.sh 'https://raw.githubusercontent.com/josephcy95/InstallNET-gcp/master/InstallNET.sh' && bash InstallNET.sh --ip-addr $gcpinternalIP --ip-mask 255.255.255.0 --ip-gate $gcpgatewayIP -dd 'https://mirrors.yuntu.ca/teddysun/en_windows2019.gz'
